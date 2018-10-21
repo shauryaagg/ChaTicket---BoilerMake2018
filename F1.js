b11ad67a682db238fe9192071d0e6c0bb589dd55
Aditya Sharma <aditya.sharma.dh@gmail.com>
2018-10-21 06:58:24 -0400

    Update F1.js
    This javascript file fetches the data from the MongoDB data storage

--- F1.js
+++ F1.js
@@ -5,6 +5,7 @@ exports = async function(){
   const raceName = await races.get({
     url: "https://tickets.formula1.com/en/3320-united-states-fom/"
   });
+  
   mongo.db("some").collection("Formula1").insertOne({ raceName });
   
   return await mongo.db("some").collection("Formula1").find({ raceName: "United States Grand Prix" }).toArray();
